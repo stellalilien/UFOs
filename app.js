d8ed3e1efa18d44ede780429f6442b2a5ab8ad52
stellalilien <94754972+stellalilien@users.noreply.github.com>
2022-03-08 01:04:25 -0800

    table set up

---
+++ app.js
@@ -0,0 +1,25 @@
+// import the data from data.js
+const tableData = data;
+
+// reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // clear out any existing data
+    tbody.html("");
+}
+
+// loop through each object in the data
+// and append a row and cells for each value in the row
+data.forEach((dataRow) => {
+    // append a row to the table body
+    let row = tbody.append("tr");
+
+    // loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+    }
+    );
+});
